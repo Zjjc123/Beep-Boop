139217fcb780a97a355ec1ff403ee15467ca7aec
Jason Zhang <zjjc123@hotmail.com>
2020-06-29 14:16:23 -0700

    Basic Sync Movement and Rotation

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using BeardedManStudios.Forge.Networking.Unity;
+
+public class GameController : MonoBehaviour
+{
+    private void Start()
+    {
+        NetworkManager.Instance.InstantiatePlayer();
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
 
-public class Movement : MonoBehaviour
+public class Movement : PlayerBehavior
 {
     [SerializeField]
     private float speed = 5f;
@@ -10,21 +11,30 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
-    Vector2 mousePos;
 
     private void Update()
     {
+        // If not the owner (other players) 
+        if (!networkObject.IsOwner)
+        {
+            // Sync transform
+            transform.position = networkObject.position;
+            transform.rotation = networkObject.rotation;
+
+            return;
+        }
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
-
-    private void FixedUpdate()
-    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+
+        // Update this object on the network
+        networkObject.position = transform.position;
+        networkObject.rotation = transform.rotation;
     }
 }
