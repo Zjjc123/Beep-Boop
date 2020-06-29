1fbb7b2c00272fbb615fc16f7eb602af22eedab0
Jason Zhang <zjjc123@hotmail.com>
2020-06-28 23:54:41 -0700

    Basic Map and Player Movment

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
