58967242ae55f7f65370e864208f5d45622d815e
Jason Zhang <zjjc123@hotmail.com>
2020-06-29 00:25:36 -0700

    Basic Rotatino and Shooting

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,15 +10,21 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
